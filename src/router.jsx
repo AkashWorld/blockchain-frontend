b988fd38fbb47991d02e360995e4320437e19110
Khalid Akash <khalkash@gmail.com>
2019-10-11 19:27:01 -0400

    Added router (#4)

---
+++ src/router.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { LoginComponent } from "./pages/login/login";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+
+/**
+ * To learn how to use the Router: https://reacttraining.com/react-router/web/guides/quick-start
+ */
+export class RouterComponent extends React.Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <div>
+          <Switch>
+            <Route path="/login">
+              <LoginComponent></LoginComponent>
+            </Route>
+            <Route path="/sign-up"></Route>
+            <Route path="/form"></Route>
+            <Route path="/global-analytics"></Route>
+            <Route path="/user-analytics"></Route>
+            <Route path="/">
+              <div>Try /login or another route</div>
+            </Route>
+          </Switch>
+        </div>
+      </BrowserRouter>
+    );
+  }
+}
