b79f2036e49818244d4e25c63f11c4f737d581aa
root <root@Rizwan-PC.localdomain>
2019-11-11 17:43:18 -0500

    Commit has some of the login mechanisims; there things that still need to be worked out though

--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -1,8 +1,24 @@
 import React from "react";
 import logo from "../../logo.svg";
 import "./login.css";
+import Web3 from "web3";
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "@apollo/react-hooks";
 
 export class LoginComponent extends React.Component {
+  async componentDidMount() {
+    const client = new ApolloClient({ uri: "http://localhost:8080" });
+    if (window.ethereum) {
+      window.Web3 = new Web3(window.ethereum);
+      try {
+        await window.ethereum.enable();
+        let accounts = window.Web3.eth.getAccounts();
+        let account = accounts[0];
+        var signedAddr = window.Web3.eth.sign();
+      } catch {}
+    }
+  }
+
   render() {
     return (
       <div>
