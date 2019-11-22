2ac6191afecb95a0771f99e20638ca5543f5be64
SuvaShahria <suvz@PC-2.localdomain>
2019-11-21 19:55:10 -0500

    update

--- mock/graphql/resolver.js
+++ mock/graphql/resolver.js
@@ -130,6 +130,185 @@ data.bpm.forEach(val => {
   val.unixTimestamp = initTimeStamp;
 });
 
+const dataGlobal = {};
+dataGlobal.lbs = [
+  { value: 150 },
+  { value: 152 },
+  { value: 157 },
+  { value: 158 },
+  { value: 158 },
+  { value: 155 },
+  { value: 159 },
+  { value: 160 },
+  { value: 159 },
+  { value: 161 },
+  { value: 163 },
+  { value: 159 },
+  { value: 165 },
+  { value: 167 },
+  { value: 169 },
+  { value: 167 },
+  { value: 171 },
+  { value: 171 },
+  { value: 171 },
+  { value: 172 },
+  { value: 174 },
+  { value: 175 },
+  { value: 182.2 },
+  { value: 120 },
+  { value: 122 },
+  { value: 127 },
+  { value: 128 },
+  { value: 128 },
+  { value: 135 },
+  { value: 139 },
+  { value: 140 },
+  { value: 149 },
+  { value: 151 },
+  { value: 153 },
+  { value: 159 },
+  { value: 175 },
+  { value: 177 },
+  { value: 179 },
+  { value: 177 },
+  { value: 181 },
+  { value: 181 },
+  { value: 181 },
+  { value: 182 },
+  { value: 184 },
+  { value: 185 },
+  { value: 192 }
+];
+dataGlobal.inch = [
+  { value: 65 },
+  { value: 65.5 },
+  { value: 65.8 },
+  { value: 66 },
+  { value: 67.4 },
+  { value: 67.9 },
+  { value: 68 },
+  { value: 68.1 },
+  { value: 69 },
+  { value: 69.1 },
+  { value: 69.4 },
+  { value: 69.5 },
+  { value: 69.7 },
+  { value: 70 },
+  { value: 70.1 },
+  { value: 70.4 },
+  { value: 70.6 },
+  { value: 70.65 },
+  { value: 70.9 },
+  { value: 71.3 },
+  { value: 71.4 },
+  { value: 71.9 },
+  { value: 72 },
+  { value: 45 },
+  { value: 45.5 },
+  { value: 45.8 },
+  { value: 46 },
+  { value: 47.4 },
+  { value: 47.9 },
+  { value: 48 },
+  { value: 48.1 },
+  { value: 49 },
+  { value: 49.1 },
+  { value: 49.4 },
+  { value: 49.5 },
+  { value: 49.7 },
+  { value: 50 },
+  { value: 50.1 },
+  { value: 50.4 },
+  { value: 50.6 },
+  { value: 50.65 },
+  { value: 50.9 },
+  { value: 51.3 },
+  { value: 51.4 },
+  { value: 51.9 },
+  { value: 52 }
+];
+dataGlobal.bpm = [
+  { value: 90 },
+  { value: 65.5 },
+  { value: 120 },
+  { value: 110 },
+  { value: 60 },
+  { value: 68 },
+  { value: 68 },
+  { value: 71 },
+  { value: 88 },
+  { value: 81 },
+  { value: 64 },
+  { value: 73 },
+  { value: 69.7 },
+  { value: 100 },
+  { value: 98.4 },
+  { value: 95 },
+  { value: 88 },
+  { value: 73 },
+  { value: 90 },
+  { value: 79 },
+  { value: 72.4 },
+  { value: 78.9 },
+  { value: 70 },
+  { value: 90 },
+  { value: 65.7 },
+  { value: 100 },
+  { value: 92 },
+  { value: 60 },
+  { value: 68 },
+  { value: 68 },
+  { value: 81 },
+  { value: 68 },
+  { value: 81 },
+  { value: 74 },
+  { value: 66 },
+  { value: 54 },
+  { value: 100 },
+  { value: 98.4 },
+  { value: 95 },
+  { value: 88 },
+  { value: 93 },
+  { value: 60 },
+  { value: 79 },
+  { value: 72.4 },
+  { value: 78.9 },
+  { value: 70 }
+];
+initTimeStamp = 1573055843;
+dataGlobal.lbs.forEach(val => {
+  val.unit = "lb";
+  const latLongRand = Math.random() * 0.05;
+  rutgersLatLong.latitude += randomSign() * latLongRand;
+  rutgersLatLong.longitude += randomSign() * latLongRand;
+  val.latitude = rutgersLatLong.latitude;
+  val.longitude = rutgersLatLong.longitude;
+  initTimeStamp += Math.floor(Math.random() * 1000);
+  val.unixTimestamp = initTimeStamp;
+});
+initTimeStamp = 1573055843;
+dataGlobal.inch.forEach(val => {
+  val.unit = "inch";
+  const latLongRand = Math.random() * 0.05;
+  rutgersLatLong.latitude += randomSign() * latLongRand;
+  rutgersLatLong.longitude += randomSign() * latLongRand;
+  val.latitude = rutgersLatLong.latitude;
+  val.longitude = rutgersLatLong.longitude;
+  initTimeStamp += Math.floor(Math.random() * 1000);
+  val.unixTimestamp = initTimeStamp;
+});
+initTimeStamp = 1573055843;
+dataGlobal.bpm.forEach(val => {
+  val.unit = "bpm";
+  const latLongRand = Math.random() * 0.05;
+  rutgersLatLong.latitude += randomSign() * latLongRand;
+  rutgersLatLong.longitude += randomSign() * latLongRand;
+  val.latitude = rutgersLatLong.latitude;
+  val.longitude = rutgersLatLong.longitude;
+  initTimeStamp += Math.floor(Math.random() * 1000);
+  val.unixTimestamp = initTimeStamp;
+});
+
 /**
  * Resolvers for GQL
  */
@@ -208,19 +387,19 @@ module.exports.resolvers = {
       await sleep(300);
       switch (args.unit) {
         case "lb":
-          return data.lbs.slice(
-            data.lbs.length - args.start - args.count,
-            data.lbs.length - args.start
+          return dataGlobal.lbs.slice(
+            dataGlobal.lbs.length - args.start - args.count,
+            dataGlobal.lbs.length - args.start
           );
         case "inch":
-          return data.inch.slice(
-            data.inch.length - args.start - args.count,
-            data.inch.length - args.start
+          return dataGlobal.inch.slice(
+            dataGlobal.inch.length - args.start - args.count,
+            dataGlobal.inch.length - args.start
           );
         case "bpm":
-          return data.bpm.slice(
-            data.bpm.length - args.start - args.count,
-            data.bpm.length - args.start
+          return dataGlobal.bpm.slice(
+            dataGlobal.bpm.length - args.start - args.count,
+            dataGlobal.bpm.length - args.start
           );
       }
       return [];
@@ -237,20 +416,15 @@ module.exports.resolvers = {
       }
       return [];
     },
-    getValuesForUnitGlobal: async (_, args) => {
-
-      
-      
-    
-      
+    getValuesForUnitGlobal: async (_, args) => {      
       await sleep(300);
       switch (args.unit) {
         case "lb":
-          return data.inch;
+          return dataGlobal.lbs;
         case "inch":
-          return data.inch;
+          return dataGlobal.inch;
         case "bpm":
-          return data.bpm;
+          return dataGlobal.bpm;
       }
       return [];
     },
