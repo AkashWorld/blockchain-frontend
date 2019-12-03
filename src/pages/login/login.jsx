c6e689c4619924d68420c109ce7c1d821bdd9f8a
canonempire19@gmail.com <canonempire19@gmail.com>
2019-12-02 21:43:02 -0500

    the completed login page

--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -1,56 +1,92 @@
 import React from "react";
 import "./login.css";
-import Web3 from "web3";
-import { ApolloClient, gql } from "apollo-boost";
-import { ApolloProvider, useMutation, useApolloClient} from "@apollo/react-hooks";
-import { login } from '../../queries';
-
+import { useMutation } from "@apollo/react-hooks";
+import { login } from "../../queries";
+import "../../../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import { token } from "../../resources/token";
+import LoadingScreen from "react-loading-screen";
+import { useHistory } from "react-router-dom";
 
 class LoginComponent extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            id:""
-        };
-    }
-  /*async componentDidMount() {
-    const client = new ApolloClient({ uri: "http://localhost:8080" });
-    if (window.ethereum) {
-      window.Web3 = new Web3(window.ethereum);
-      try {
-        await window.ethereum.enable();
-        let accounts = window.Web3.eth.getAccounts();
-        let account = accounts[0];
-        var signedAddr = window.Web3.eth.sign();
-      } catch {}
-    }
-    }*/
-    submitForm(e) {
-        e.preventDefault();
-        this.props.login({
-            variables: {
-                id: this.state.id
-            }
-        });
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      id: ""
+    };
+  }
+  submitForm(e) {
+    e.preventDefault();
+    this.props.login({
+      variables: {
+        id: this.state.id
+      }
+    });
+  }
   render() {
     return (
-        <div>
-           <form className="field" id="add" onSubmit={this.submitForm.bind(this) }>
-                <div className="id">
-                    <label>Password:</label>
-                    <input type="password" onChange={(e) => this.setState({ id: e.target.value })} />
-                </div>    
-                <div className="button">
-                    <button type="submit">Log in</button>
-                </div>                
-            </form>
+      <div className="auth-wrapper">
+        <div className="auth-inner">
+          <form>
+            <h3>Sign In</h3>
+
+            <div className="form-group">
+              <label>Block address</label>
+              <input
+                type="password"
+                className="form-control"
+                placeholder="Enter your block address"
+                onChange={e => this.setState({ id: e.target.value })}
+              />
+            </div>
+            <div className="form-group">
+              <div className="custom-control custom-checkbox">
+                <input
+                  type="checkbox"
+                  className="custom-control-input"
+                  id="customCheck1"
+                />
+                <label className="custom-control-label" htmlFor="customCheck1">
+                  Remember me
+                </label>
+              </div>
+            </div>
+
+            <button
+              type="submit"
+              className="btn btn-primary btn-block"
+              onClick={e => this.submitForm(e)}
+            >
+              Submit
+            </button>
+            <p className="forgot-password text-right">
+              Do not have an account? <a href="/signup">Sign up</a>
+            </p>
+          </form>
+        </div>
       </div>
     );
   }
-};
+}
 export function Login() {
-    const client = useApolloClient();
-    const [user_login, { data }] = useMutation(login);
-    return <LoginComponent login={user_login}/>
+  const history = useHistory();
+  const [userLogin, { loading }] = useMutation(login, {
+    onCompleted({ verify }) {
+      const { address } = verify;
+      console.log(address);
+      localStorage.setItem(token, address);
+      history.push("/dashboard");
+    }
+  });
+  if (loading) {
+    return (
+      <LoadingScreen
+        loading={true}
+        bgColor="#66ccff"
+        spinnerColor="#9ee5f8"
+        textColor="#ffffff"
+        text="Verifying"
+      />
+    );
+  }
+  return <LoginComponent history={history} login={userLogin} />;
 }
--- src/router.jsx
+++ src/router.jsx
@@ -1,8 +1,8 @@
 import React from "react";
-import { Login} from "./pages/login/login";
-import { HashRouter, Switch, Route } from "react-router-dom";
-import { DataEntryForm } from "./pages/data-entry/data-entry";
+import { Login } from "./pages/login/login";
+import { Switch, Route, BrowserRouter } from "react-router-dom";
 import { UserAnalyticsPage } from "./pages/user-descriptor/user-analytics";
+import { SignUp } from "./pages/signup/signup";
 /**
  * To learn how to use the Router: https://reacttraining.com/react-router/web/guides/quick-start
 
@@ -11,31 +11,14 @@ export class RouterComponent extends React.Component {
     render() {
         return (
             <div>
-                <HashRouter>
+                <BrowserRouter>
                     <Switch>
-                        <Route path="/" component={Login}/>
+                        <Route exact path="/" component={Login} />
+                        <Route path="/signup" component={SignUp} />
+                        <Route path="/dashboard" component={UserAnalyticsPage} />
                     </Switch>
-                </HashRouter>
+                </BrowserRouter>
             </div>
     );
-    /*return (
-      <HashRouter>
-            <div>
-                <Route path="/user-analytics" component={UserAnalyticsPage}></Route>
-                <Switch>
-                    <Route path="/login" component={LoginComponent} />
-              <LoginComponent/>
-            <Route path="/sign-up"></Route>
-            <Route path="/form">
-              <DataEntryForm></DataEntryForm>
-            </Route>
-                    <Route path="/global-analytics"></Route>
-            
-            <Route exact path="/">
-            </Route>
-          </Switch>
-        </div>
-      </HashRouter>
-    );*/
   }
 }
